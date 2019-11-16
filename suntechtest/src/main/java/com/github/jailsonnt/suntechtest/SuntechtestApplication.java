0f457c4ead89f7c7805a6f2042fe6f7cb767ed29
Jailson Nicoletti <jailsonnt@gmail.com>
2019-11-16 14:43:03 -0300

    suntechtest - CRUD Usuário - Criação do projeto
    +Criado projeto SpringBoot com Maven e JDK 1.8

---
+++ suntechtest/src/main/java/com/github/jailsonnt/suntechtest/SuntechtestApplication.java
@@ -0,0 +1,37 @@
+package com.github.jailsonnt.suntechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EntityScan(
+		basePackages = {
+				"com.github.jailsonnt.suntechtest.dao"})
+public class SuntechtestApplication {
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuntechtestApplication.class, args);
+	}
+
+}
---
+++ suntechtest/src/test/java/com/github/jailsonnt/suntechtest/SuntechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jailsonnt.suntechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuntechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
